08c600883ba3b97c7d0cd9796a6e828bfd984729
Chris Whelan <cwhelan@gmail.com>
2012-02-09 23:13:51 -0800

    Add commands to read sequences into HDFS, split deflated output, implement Tool

---
+++ src/main/java/edu/ohsu/sonmezsysbio/svpipeline/command/CommandReadPairedEndFilesIntoHDFS.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.svpipeline.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 2/4/12
+ * Time: 3:38 PM
+ */
+public class CommandReadPairedEndFilesIntoHDFS {
+}
---
+++ src/main/java/edu/ohsu/sonmezsysbio/svpipeline/command/CommandSplitDeflatedOutput.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.svpipeline.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 2/6/12
+ * Time: 10:29 PM
+ */
+public class CommandSplitDeflatedOutput {
+}
