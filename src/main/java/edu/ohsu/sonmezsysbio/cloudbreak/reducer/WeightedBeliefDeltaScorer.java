22b4336c8053ad9c862d7fe9c0f6d265427a73f6
Chris Whelan <cwhelan@gmail.com>
2012-08-24 15:33:55 -0700

    Weighted Belief Delta Scorer

---
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/WeightedBeliefDeltaScorer.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.reducer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 8/23/12
+ * Time: 9:52 AM
+ */
+public class WeightedBeliefDeltaScorer {
+}
---
+++ src/test/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/WeightedBeliefDeltaScorerTest.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.reducer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 8/23/12
+ * Time: 10:27 AM
+ */
+public class WeightedBeliefDeltaScorerTest {
+}
