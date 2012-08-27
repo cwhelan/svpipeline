ee231484f08a1de0a7a4da751cf2f83df87ba9d5
Chris Whelan <cwhelan@gmail.com>
2012-08-27 14:53:29 -0700

    self information

--- src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/WeightedBeliefDeltaScorer.java
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/WeightedBeliefDeltaScorer.java
@@ -1,10 +1,96 @@
 package edu.ohsu.sonmezsysbio.cloudbreak.reducer;
 
+import edu.ohsu.sonmezsysbio.cloudbreak.ReadGroupInfo;
+import edu.ohsu.sonmezsysbio.cloudbreak.io.ReadPairInfo;
+import org.apache.commons.math3.distribution.LogNormalDistribution;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.log4j.Logger;
+
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by IntelliJ IDEA.
  * User: cwhelan
  * Date: 8/23/12
  * Time: 9:52 AM
  */
-public class WeightedBeliefDeltaScorer {
+public class WeightedBeliefDeltaScorer implements ReadPairInfoScorer {
+
+    private static org.apache.log4j.Logger log = Logger
+            .getLogger(WeightedBeliefDeltaScorer.class);
+
+    public double reduceReadPairInfos(Iterator<ReadPairInfo> values, Map<Short, ReadGroupInfo> readGroupInfos) {
+        LogNormalDistribution logNormalDistribution = new LogNormalDistribution(6, 0.6);
+
+        double pDeletionPrior = Math.log(2432.0 / 2700000000.0);
+        double pNoDeletionPrior = Math.log(1 - 2432.0 / 2700000000.0);
+
+        // keeping these values outside of the log domain since they are a sum and involve log subtraction
+        double weightedSumOfDeletionSurprises = 2432.0 / 2700000000.0;
+        double weightedSumOfNoDeletionSurprises = 1 - 2432.0 / 2700000000.0;
+        log.debug("weightedSumOfDeletionDeltas: " + weightedSumOfDeletionSurprises);
+        log.debug("weightedSumOfNoDeletionDeltas: " + weightedSumOfNoDeletionSurprises);
+
+        while (values.hasNext()) {
+            ReadPairInfo readPairInfo = values.next();
+            int insertSize = readPairInfo.insertSize;
+            double pMappingCorrect = readPairInfo.pMappingCorrect;
+            log.debug("processing " + readPairInfo);
+            short readGroupId = readPairInfo.readGroupId;
+
+            ReadGroupInfo readGroupInfo = readGroupInfos.get(readGroupId);
+            int targetIsize = readGroupInfo.isize;
+            int targetIsizeSD = readGroupInfo.isizeSD;
+
+            NormalDistribution normalDistribution = new NormalDistribution(targetIsize, targetIsizeSD);
+            double pISgivenDeletion = Math.log(logNormalDistribution.density(insertSize));         // todo add fragment size
+            double pISgivenNoDeletion = Math.log(normalDistribution.density(insertSize));
+            log.debug("pISgivenDeletion: " + pISgivenDeletion);
+            log.debug("pISgivenNoDeletion: " + pISgivenNoDeletion);
+
+            if (insertSize > targetIsize + 10 * targetIsizeSD) {
+                pISgivenNoDeletion = Math.log(normalDistribution.density(targetIsize + 10 * targetIsizeSD));
+                log.debug("adj pISgivenNoDeletion: " + pISgivenNoDeletion);
+            }
+            double pDeletionGivenIS = pDeletionPrior + pISgivenDeletion;
+            double pNoDeletionGivenIS = pNoDeletionPrior + pISgivenNoDeletion;
+
+            double pDeletionSurprise = Math.exp(pMappingCorrect) * pDeletionGivenIS;
+            double pNoDeletionSurprise = Math.exp(pMappingCorrect) * pNoDeletionGivenIS;
+
+            log.debug("pDeletionDelta: " + pDeletionSurprise);
+            log.debug("pNoDeletionDelta: " + pNoDeletionSurprise);
+            weightedSumOfDeletionSurprises += pDeletionSurprise;
+            weightedSumOfNoDeletionSurprises += pNoDeletionSurprise;
+        }
+        log.debug("sumdiff: " + (weightedSumOfDeletionSurprises - weightedSumOfNoDeletionSurprises));
+        return weightedSumOfNoDeletionSurprises / weightedSumOfDeletionSurprises;
+
+    }
+
+    // from https://facwiki.cs.byu.edu/nlp/index.php/Log_Domain_Computations
+    public static double logAdd(double logX, double logY) {
+        // 1. make X the max
+        if (logY > logX) {
+            double temp = logX;
+            logX = logY;
+            logY = temp;
+        }
+        // 2. now X is bigger
+        if (logX == Double.NEGATIVE_INFINITY) {
+            return logX;
+        }
+        // 3. how far "down" (think decibels) is logY from logX?
+        //    if it's really small (20 orders of magnitude smaller), then ignore
+        double negDiff = logY - logX;
+        if (negDiff < -20) {
+            return logX;
+        }
+        // 4. otherwise use some nice algebra to stay in the log domain
+        //    (except for negDiff)
+        return logX + java.lang.Math.log(1.0 + java.lang.Math.exp(negDiff));
+    }
+
+
 }
