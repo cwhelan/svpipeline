bb48f7bd8645498e850da448e2e7666617d71a6c
Chris Whelan <cwhelan@gmail.com>
2012-10-02 12:44:34 -0700

    switch to snappy compression for reads

--- src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/mapper/SingleEndAlignmentMapper.java
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/mapper/SingleEndAlignmentMapper.java
@@ -48,6 +48,11 @@ public abstract class SingleEndAlignmentMapper extends MapReduceBase implements 
         String line = value.toString();
         String[] fields = line.split("\t");
 
+        if (fields[1].length() != fields[3].length()) {
+            System.err.println("Warning; mismatching seq and qual lengths in record " + key.toString() + "!");
+            System.err.println(fields[1]);
+            System.err.println(fields[3]);
+        }
         s1FileWriter.write(fields[0] + "\n");
         s1FileWriter.write(fields[1] + "\n");
         s1FileWriter.write(fields[2] + "\n");
