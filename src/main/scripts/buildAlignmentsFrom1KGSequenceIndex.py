ea5c1a3b8bb08860be89737c313ee39c910f5a86
Chris Whelan <cwhelan@gmail.com>
2012-10-03 10:28:39 -0700

    script to create condor dag for loading and aligning 1KG sequences

--- src/main/scripts/buildAlignmentsFrom1KGSequenceIndex.py
+++ src/main/scripts/buildAlignmentsFrom1KGSequenceIndex.py
@@ -23,7 +23,7 @@ for line in seq_index:
     insert_size = fields[20]
     pair_file_name = fields[24]
     if (not (read_group in loaded_groups) and pair_file_name != ""):
-        print "processing read group {}".format(read_group)
+        print "processing read group {0}".format(read_group)
         dag_file.write("JOB {0} loadAndAlign.desc\n".format(job_num))
         dag_file.write("VARS {0} read_group={1}\n".format(job_num, read_group))
         dag_file.write("VARS {0} data_dir={1}\n".format(job_num, data_dir))
