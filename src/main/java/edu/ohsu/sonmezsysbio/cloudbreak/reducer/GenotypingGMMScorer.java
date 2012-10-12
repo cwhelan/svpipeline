d077cc042f665ad71937edcdc94073150b36e075
Chris Whelan <cwhelan@gmail.com>
2012-10-12 15:56:25 -0700

    implementation of GMM scorer

---
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/GenotypingGMMScorer.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.reducer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 10/10/12
+ * Time: 9:17 PM
+ */
+public class GenotypingGMMScorer {
+}
---
+++ src/test/java/edu/ohsu/sonmezsysbio/cloudbreak/reducer/GenotypingGMMScorerTest.java
@@ -0,0 +1,16 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.reducer;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 10/12/12
+ * Time: 10:11 AM
+ */
+public class GenotypingGMMScorerTest {
+    @Test
+    public void testNnclean() throws Exception {
+
+    }
+}
