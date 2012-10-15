9980b84bf68669c99f5c2104f09b5006e4fb21f8
Chris Whelan <cwhelan@gmail.com>
2012-10-15 13:49:30 -0700

    process SAM alignment records; read them into HDFS from a BAM

---
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/command/CommandReadSAMFileIntoHDFS.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.command;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 10/15/12
+ * Time: 10:50 AM
+ */
+public class CommandReadSAMFileIntoHDFS {
+}
---
+++ src/main/java/edu/ohsu/sonmezsysbio/cloudbreak/io/SAMAlignmentReader.java
@@ -0,0 +1,10 @@
+package edu.ohsu.sonmezsysbio.cloudbreak.io;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwhelan
+ * Date: 10/15/12
+ * Time: 1:36 PM
+ */
+public class SAMAlignmentReader {
+}
