5344807c1985ff4767b147e1511beb4a4c48435b
Chris Whelan <cwhelan@gmail.com>
2012-11-27 16:14:00 -0800

    add haplotype info

--- src/main/scripts/simulate_diploid_reads.sh
+++ src/main/scripts/simulate_diploid_reads.sh
@@ -16,8 +16,8 @@ OUTPUT_PREFIX=$3
 # -R: no indels (indels already applied to reference)
 # -H: haploid mode
 
-dwgsim -e 0.02-0.02 -E 0.02-0.02 -d 100 -s 30 -1 100 -2 100 -C 10 -r 0.0010 -R 0 -H $HAP1_REF $HAP1_REF
-dwgsim -e 0.02-0.02 -E 0.02-0.02 -d 100 -s 30 -1 100 -2 100 -C 10 -r 0.0010 -R 0 -H $HAP2_REF $HAP2_REF
+dwgsim -e 0.02-0.02 -E 0.02-0.02 -d 100 -s 30 -1 100 -2 100 -C 10 -r 0.0010 -R 0 -H -P hap1 $HAP1_REF $HAP1_REF
+dwgsim -e 0.02-0.02 -E 0.02-0.02 -d 100 -s 30 -1 100 -2 100 -C 10 -r 0.0010 -R 0 -H -P hap2 $HAP2_REF $HAP2_REF
 
 cat ${HAP1_REF}.bwa.read1.fastq ${HAP2_REF}.bwa.read1.fastq | gzip -c > ${OUTPUT_PREFIX}.read1.fastq.gz
 cat ${HAP1_REF}.bwa.read2.fastq ${HAP2_REF}.bwa.read2.fastq | gzip -c > ${OUTPUT_PREFIX}.read2.fastq.gz
