5f263409864a00afd3ed9a9d07db265fc597ac42
Chris Whelan <cwhelan@gmail.com>
2012-11-29 16:32:40 -0800

    fix eval scripts

--- src/main/scripts/evalWigFileAtThreshold.py
+++ src/main/scripts/evalWigFileAtThreshold.py
@@ -48,6 +48,6 @@ for line in open_file(temp_file_name):
     bed_line = line.strip()
     bed_lines.append(bed_line)
         
-(qualified_calls, matches, short_calls) = evalBedFile.eval_bed(truth_filename, calls_gte_threshold)
+(qualified_calls, matches, short_calls) = evalBedFile.eval_bed(truth_filename, bed_lines)
 tpr = float(matches) / (long_calls + qualified_calls)
 print "\t".join(map(str, [q, qualified_calls, matches, 0, 0, short_calls, tpr]))
