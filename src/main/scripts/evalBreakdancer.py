c68ccd148c9fb598cc462b978b4aba971d050e3e
Chris Whelan <cwhelan@gmail.com>
2013-01-03 14:33:42 -0800

    change evaulation of breakdancer to use number of pairs

--- src/main/scripts/evalBreakdancer.py
+++ src/main/scripts/evalBreakdancer.py
@@ -20,7 +20,7 @@ for line in breakdancer_file:
     if line.startswith("#"):
         continue
     fields = line.split("\t")
-    score = float(fields[8])
+    score = float(fields[9])
     score_values.append(score)
 breakdancer_file.close()
 
@@ -32,16 +32,22 @@ for v in unique_score_values:
     calls_gte_threshold = []
     breakdancer_file = open(breakdancer_filename, "r")
     non_del_calls = 0
+    # sometimes Breakdancer makes nonsensical calls; deletions with ends on different chromosomes, we have to
+    # track those separately here
+    bad_calls = 0
     for line in breakdancer_file:
 #        print line
         fields = line.split("\t")
         if line.startswith("#"):
 #            print "comment!"
             continue
-        if float(fields[8]) >= v:
+        if float(fields[9]) >= v:
 #            print "gte v"
-            if not (fields[6] == "DEL") or (fields[0] != fields[3]):
+            if not (fields[6] == "DEL"):
                 non_del_calls += 1
+                continue
+            if (fields[0] != fields[3]):
+                bad_calls += 1
                 continue
             sv_len = int(fields[7])
             # sys.stderr.write("len: " + str(sv_len) + "\n")
@@ -55,6 +61,7 @@ for v in unique_score_values:
         bed_lines.append(bed_line)
 
     (qualified_calls, matches, short_calls) = evalBedFile.eval_bed(truth_filename, bed_lines)
+    qualified_calls += bad_calls
     tpr = float(matches) / (qualified_calls)
     print "\t".join(map(str, [v, qualified_calls, matches, non_del_calls, short_calls, tpr]))
     
