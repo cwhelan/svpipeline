34b717eb2235f2bf184fe486810db30a7669e3b3
Chris Whelan <cwhelan@gmail.com>
2013-02-15 15:02:13 -0800

    final version of recomb seq submission

--- src/main/scripts/recomb.R
+++ src/main/scripts/recomb.R
@@ -2,6 +2,7 @@ library(GenomicRanges)
 library(rtracklayer)
 library(plyr)
 library(RColorBrewer)
+library(VariantAnnotation)
 
 drawPerfLine <- function(perf, col, lineType, maxFP) {
   calls <- c(perf$Calls, min(perf$Calls) - min(perf$TP), 0)
@@ -57,13 +58,13 @@ processPredictions <- function(name, predfile, trueDels) {
 #chr2 gt 50 100bp diploid
 totalDels <- 400
 cloudbreak <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/chr2mwf5_readGroupsRazerS3_new3_i94_s99_m1000_None_None_25000_25_sam_6_32fc04d_-1_-1_3_lrHeterozygous.short40.perf.txt', header=TRUE, sep="\t")
-breakdancer <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/breakdancer/bwa_new3_sort_35_2_3.bd_out.small40.perf.txt', header=TRUE)
+breakdancer <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/breakdancer/bwa_new3_sort_35_2_3.bd_out.short40.pairthresh.perf.txt', header=TRUE)
 gasv <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/GASVPro/BAMToGASV.gasvpro.in.ALL.MCMCThreshold.clusters.pruned.clusters.short40.perf.txt', header=TRUE)
 delly <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/delly/delly_q0_c5_del_bwa_new3.small40.perf.txt', header=TRUE)
 pindel <- read.table('~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/pindel/bwa_new3.pindel.noshort.short40.perf.txt', header=TRUE)
 
 perfsList <- list(breakdancer=breakdancer, gasv=gasv, pindel=pindel, delly=delly, cloudbreak=cloudbreak)
-pdf('~/Documents/svpipeline/manuscript/CHR2SIM_ROC_NEW.pdf')
+pdf('~/Documents/svpipeline/manuscript/CHR2SIM_ROC_NEW.pdf', width=10)
 par(xpd=T, mar=par()$mar+c(0,0,0,7))
 plotROC(perfsList, c("Breakdancer","GASVPro", "Pindel", "DELLY", "Cloudbreak"), totalDels, "Venter deletions simulated in diploid chr2",legendLoc=xy.coords(425,200), maxTP=350)
 dev.off()
@@ -91,7 +92,7 @@ mcols(trueDels[as.matrix(findOverlaps(repMask,trueDels))[,2]])$repMask <- 1
 
 cb <- processPredictions("Cloudbreak", '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/chr2mwf5_readGroupsRazerS3_new3_i94_s99_m1000_None_None_25000_25_sam_6_32fc04d_-1_-1_3_lrHeterozygous.gt1p68.mufilt.hits.txt', trueDels)
 
-bd <- processPredictions('Breakdancer', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/breakdancer/bwa_new3_sort_35_2_3.bd_out.short40.gte44.hits.txt', trueDels)
+bd <- processPredictions('Breakdancer', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/breakdancer/bwa_new3_sort_35_2_3.bd_out.pairgte3.hits.txt', trueDels)
 
 gasv <- processPredictions('GASVPro', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/GASVPro/BAMToGASV.gasvpro.in.ALL.MCMCThreshold.clusters.pruned.clusters.short40.gte8.hits.txt', trueDels)
 
@@ -136,22 +137,49 @@ names(extraData) <- c('predchrom', 'predstart', 'predend', 'peaknum', 'maxscore'
 
 cbpredsWithExtraData <- merge(as.data.frame(cb), extraData)
 
+hapCM <- table(cbpredsWithExtraData$hap, cbpredsWithExtraData$avgW < .2)
+print(hapCM)
+print((hapCM[1,1] + hapCM[2,2]) / sum(hapCM))
 
 # NA18507
 
 trueDelsNA18507 <- import.bed('~/Documents/gene_rearrange/svpipeline/NA18507/MillsDIP1KG_merged.bed', asRangedData=FALSE)
 
+mcols(trueDelsNA18507)$tok <- paste(seqnames(trueDelsNA18507), ':', start(trueDelsNA18507)-1, '-', end(trueDelsNA18507), sep="")
+
 mcols(trueDelsNA18507)$sizeclasses <- sizeclasses(start(trueDelsNA18507), end(trueDelsNA18507))
+
+repMaskHg19 <- import.bed('~/genomes/1kg/hg19/repeatmasker.bed', asRangedData=FALSE)
+
+mcols(trueDelsNA18507)$repMask <- 0
+mcols(trueDelsNA18507[as.matrix(findOverlaps(repMaskHg19,trueDelsNA18507))[,2]])$repMask <- 1
 
 trueDelsNA18507Gte40 <- trueDelsNA18507[end(trueDelsNA18507) - start(trueDelsNA18507) >= 40]
 trueDelsNA18507Sizes <- table(trueDelsNA18507Gte40$sizeclasses)
 
+trueDelRepmask <- table(trueDelsNA18507Gte40$repMask)
+
+na185071KGVariants <- readVcf('~/Documents/gene_rearrange/svpipeline/NA18507/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.NA18507.vcf.gz', "hg19")
+
+mcols(rowData(na185071KGVariants))$gt <- as.factor(geno(na185071KGVariants)$GT)
+mcols(rowData(na185071KGVariants))$hap <- ifelse(mcols(rowData(na185071KGVariants))$gt == "1|1", 2, 1)
+
+na185071KGDels <- rowData(na185071KGVariants)[nchar(fixed(na185071KGVariants)$REF) > nchar(unlist(fixed(na185071KGVariants)$ALT))]
+
+na185071KGDels <- na185071KGDels[width(na185071KGDels) >= 40]
+
+mcols(trueDelsNA18507)$hap <- NA
+
+ol <- findOverlaps(trueDelsNA18507, na185071KGDels) 
+
+mcols(trueDelsNA18507[as.matrix(ol)[,1]])$hap <- mcols(na185071KGDels[as.matrix(ol)[,2]])$hap
+
 # ROC curve
 totalDels <- 10000
 cloudbreak <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/na18507final_readGroupsRazerS3_i94_s99_m1000_None_None_25000_25_sam_6_67eedd7_-1_-1_3_lrHeterozygous.allsvs.short40.perf.txt', header=TRUE, sep="\t")
-breakdancer <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/breakdancer/NA18507_35_2_4.bd.perf.txt', header=TRUE)
+breakdancer <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/breakdancer/NA18507_35_2_3.bd_out.short40.pairthresh.perf.txt', header=TRUE)
 gasv <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/GASVPro/GASVPro.short40.MillsDIP1KG_merged.perf.txt', header=TRUE)
-delly <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/delly/NA18507.delly_q0_c5_del.perf.txt', header=TRUE)
+delly <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/delly/NA18507.delly_q0_c5_del.short40.perf.txt', header=TRUE)
 pindel <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/pindel/bwa_pindel_D.noshort.perf.txt', header=TRUE)
 
 perfsList <- list(breakdancer=breakdancer, gasv=gasv, pindel=pindel, delly=delly, cloudbreak=cloudbreak)
@@ -160,21 +188,39 @@ par(xpd=T, mar=par()$mar+c(0,0,0,7))
 plotROC(perfsList, c("Breakdancer","GASVPro", "Pindel", "DELLY", "Cloudbreak"), totalDels, "NA18507", legendLoc=xy.coords(10750,700), maxTP=1300, sim=FALSE)
 dev.off()
 
+# break down predictons
 
-cbHitsNA18507 <- processPredictions("Cloudbreak", '~/Documents/gene_rearrange/svpipeline/NA18507/na18507f', trueDelsNA18507)
+cbHitsNA18507 <- processPredictions("Cloudbreak", '~/Documents/gene_rearrange/svpipeline/NA18507/na18507final_readGroupsRazerS3_i94_s99_m1000_None_None_25000_25_sam_6_67eedd7_-1_-1_3_lrHeterozygous.gte2pt072.mufilt.hits.txt', trueDelsNA18507)
 
-bdHitsNA18507 <- processPredictions('Breakdancer', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/breakdancer/bwa_new3_sort_35_2_3.bd_out.short40.gte44.hits.txt', trueDelsNA18507)
+bdHitsNA18507 <- processPredictions('Breakdancer', '~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/breakdancer/NA18507_35_2_3.bd_out.pairgte4.hits.txt', trueDelsNA18507)
 
-gasvHitsNA18507 <- processPredictions('GASVPro', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/GASVPro/BAMToGASV.gasvpro.in.ALL.MCMCThreshold.clusters.pruned.clusters.short40.gte8.hits.txt', trueDelsNA18507)
+gasvHitsNA18507 <- processPredictions('GASVPro', '~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/GASVPro/BAMToGASV.gasvpro.in.ALL.MCMCThreshold.clusters.pruned.clusters.gte9pt8.hits.txt', trueDelsNA18507)
 
-dellyHitsNA18507 <- processPredictions('DELLY', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/delly/bwa_new3.delly_q0_c5_del.short40.gte4.hits.txt', trueDelsNA18507)
+dellyHitsNA18507 <- processPredictions('DELLY', '~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/delly/NA18507.delly_q0_c5_del.gte5.hits.txt', trueDelsNA18507)
 
-pindelHitsNA18507 <- processPredictions('Pindel', '~/Documents/gene_rearrange/svpipeline/venter_chr2_100bp_dip/final_predictions/pindel/bwa_new3.pindel.noshort.gte18.hits.txt', trueDelsNA18507)
+pindelHitsNA18507 <- processPredictions('Pindel', '~/Documents/gene_rearrange/svpipeline/NA18507/final_predictions/pindel/bwa_pindel.gte22.hits.txt', trueDelsNA18507)
 
+allpredsNA18507 <- rbind(as.data.frame(cbHitsNA18507),as.data.frame(bdHitsNA18507),as.data.frame(gasvHitsNA18507),as.data.frame(dellyHitsNA18507),as.data.frame(pindelHitsNA18507))
+allpredsNA18507 <- ddply(allpredsNA18507, .(tok), function(x) { cbind(x, list(discoveries=rep(dim(x)[1],dim(x)[1]))) })
+
+predsBySizeNA18507 <- table(allpredsNA18507[,c('name','sizeclasses')])
+exBySizeNA18507 <- table(allpredsNA18507[which(allpredsNA18507$discoveries == 1),c('name','sizeclasses')])
+
+predsByRepMaskNA18507 <- table(allpredsNA18507[,c('name','repMask')])
+exByRepMaskNA18507 <- table(allpredsNA18507[which(allpredsNA18507$discoveries == 1),c('name','repMask')])
 
 # genotyping 
 
-mcols(trueDelsNA18507)$hap <- NA
+extraDataNA18507 <- read.table('~/Documents/gene_rearrange/svpipeline/NA18507/na18507final_readGroupsRazerS3_i94_s99_m1000_None_None_25000_25_sam_6_67eedd7_-1_-1_3_lrHeterozygous.gte2pt072.extraInfo.muFilter.bed', skip=1)
+names(extraDataNA18507) <- c('predchrom', 'predstart', 'predend', 'peaknum', 'maxscore', 'avgMu', 'minMu', 'maxMu', 'avgW', 'minW', 'maxW', 'avgCC', 'minCC', 'maxCC', 'avgc1', 'minC1', 'maxC1', 'avgc2', 'minC2', 'maxC2', 'avgwc1', 'minwC1', 'maxwC1', 'avgwc2', 'minwC2', 'maxwC2')
+
+cbpredsWithExtraDataNA18507 <- merge(as.data.frame(cbHitsNA18507), extraDataNA18507)
+
+cbpredsWithExtraDataNA18507Genotyped <- cbpredsWithExtraDataNA18507[!is.na(cbpredsWithExtraDataNA18507$hap),]
+
+hapCMNA18507 <- table(cbpredsWithExtraDataNA18507$avgW < .2, cbpredsWithExtraDataNA18507$hap)
+print(hapCMNA18507)
+print((hapCMNA18507[1,1] + hapCMNA18507[2,2]) / sum(hapCMNA18507))
 
 millsDelsWithGenotypes <- read.table('~/Documents/Papers2/Articles/2011/Mills/Supplemental/dels_gt40_with_genotypes.csv', header=TRUE, sep="\t")
 millsGenotypeData <- read.table('~/Documents/Papers2/Articles/2011/Mills/Supplemental/Mills_genotypes_NA18507_with_ids_gt40.txt', header=TRUE, sep="\t")
@@ -182,7 +228,3 @@ millsGenotypeData <- read.table('~/Documents/Papers2/Articles/2011/Mills/Supplem
 millsFullGenotypedDels <- merge(millsDelsWithGenotypes,millsGenotypeData)
 
 millsGenotypedRanges <- GRanges(seqnames=substr(millsFullGenotypedDels$CHR,4,length(millsFullGenotypedDels$CHR)), ranges=IRanges(start=millsFullGenotypedDels$START, end=millsFullGenotypedDels$STOP), mcols=DataFrame(hap=millsFullGenotypedDels$SED00002_NA18507.CEL))
-
-hg18ToHg19chain <- import.chain("~/genomes/ucsc/hg18/hg18ToHg19.over.chain.gz")
-
-millsGenotypedRangesHg19 <- 
